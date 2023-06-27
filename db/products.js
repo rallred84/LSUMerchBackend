dd35878f4e8fa27c5734910d209d709b89c9940c
sweakleyw <sweakleyworks@gmail.com>
2023-06-26 23:27:10 -0400

    products

--- db/products.js
+++ db/products.js
@@ -1,4 +1,4 @@
-const client = require('./client');
+const client = require("./client");
 
 async function createProduct({ name, description, price, quantity, size }) {
   try {
@@ -21,4 +21,89 @@ async function createProduct({ name, description, price, quantity, size }) {
   }
 }
 
-module.exports = { createProduct };
+async function getAllProducts() {
+  try {
+    const { rows: products } = await client.query(
+      `
+    SELECT * 
+    FROM products 
+    `
+    );
+
+    return products;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getProductById(id) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
+    SELECT * 
+    FROM products 
+    WHERE id=$1;
+    `,
+      [id]
+    );
+
+    return product;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function updateProduct({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
+    UPDATE routines
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+  `,
+      Object.values(fields)
+    );
+
+    return product;
+  } catch (error) {
+    console.error(err);
+  }
+}
+
+async function destroyProduct(id) {
+  try {
+    const { rows: products } = await client.query(
+      `
+      DELETE FROM products 
+      WHERE id=$1
+      RETURNING *;
+    `,
+      [id]
+    );
+
+    return products;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  destroyProduct,
+};

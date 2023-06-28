3211342047e9c8c3a6d111f00da6af4ee567f83c
sweakleyw <sweakleyworks@gmail.com>
2023-06-28 17:57:30 -0400

    basic endpoint functions orders, reviews

--- db/orders.js
+++ db/orders.js
@@ -21,4 +21,68 @@ async function createOrder({ price, hasShipped, isComplete }) {
   }
 }
 
-module.exports = { createOrder };
+async function getAllOrders() {
+  try {
+    const { rows: orders } = await client.query(
+      `
+    SELECT * 
+    FROM orders 
+    `
+    );
+
+    return orders;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getOrdersById(id) {
+  try {
+    const {
+      rows: [order],
+    } = await client.query(
+      `
+    SELECT * 
+    FROM orders 
+    WHERE id=$1;
+    `,
+      [id]
+    );
+
+    return order;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function updateOrder({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [order],
+    } = await client.query(
+      `
+    UPDATE orders
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+  `,
+      Object.values(fields)
+    );
+
+    return order;
+  } catch (error) {
+    console.error(err);
+  }
+}
+
+//get by email when tables joined
+
+module.exports = { createOrder, getAllOrders, getOrdersById, updateOrder };
--- db/reviews.js
+++ db/reviews.js
@@ -21,4 +21,92 @@ async function createReview({ message, rating }) {
   }
 }
 
-module.exports = { createReview };
+async function getAllReviews() {
+  try {
+    const { rows: reviews } = await client.query(
+      `
+    SELECT * 
+    FROM reviews 
+    `
+    );
+
+    return reviews;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getReviewById(id) {
+  try {
+    const {
+      rows: [review],
+    } = await client.query(
+      `
+    SELECT * 
+    FROM reviews 
+    WHERE id=$1;
+    `,
+      [id]
+    );
+
+    return review;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function updateReview({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [review],
+    } = await client.query(
+      `
+    UPDATE products
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+  `,
+      Object.values(fields)
+    );
+
+    return review;
+  } catch (error) {
+    console.error(err);
+  }
+}
+
+async function destroyReview(id) {
+  try {
+    const { rows: reviews } = await client.query(
+      `
+      DELETE FROM reviews 
+      WHERE id=$1
+      RETURNING *;
+    `,
+      [id]
+    );
+
+    return reviews;
+  } catch (error) {
+    console.error(err);
+  }
+}
+
+//delete from products when table joined
+//get reviews by product when tables joined
+
+module.exports = {
+  createReview,
+  getAllReviews,
+  getReviewById,
+  updateReview,
+  destroyReview,
+};

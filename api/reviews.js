c503ffa042049c0421f8d64d4656b71e4ee3a4ce
sweakleyw <sweakleyworks@gmail.com>
2023-06-29 23:37:29 -0400

    reviews api

--- api/reviews.js
+++ api/reviews.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const reviewsRouter = express.Router();
 const jwt = require("jsonwebtoken");
-const { createReview } = require("../db");
+
+const { createReview, getReviewById, updateReview } = require("../db");
+const { requireUser } = require("./utils");
+
+// GET /reviews/:productId
+//join prodId to products to get reviews for product
 
 // POST /reviews/:productId
 
-reviewsRouter.post("/:productId", async (req, res, next) => {
+reviewsRouter.post("/:productId", requireUser, async (req, res, next) => {
   const { productId } = req.params;
   const { message, rating } = req.body;
   try {
@@ -28,6 +33,37 @@ reviewsRouter.post("/:productId", async (req, res, next) => {
 });
 
 // PATCH /reviews/:productId
+
+reviewsRouter.patch("/:productId", requireUser, async (req, res, next) => {
+  const { productId } = req.params;
+  const { message, rating } = req.body;
+
+  try {
+    const updatedReview = await updateReview({
+      creatorId: req.user.id,
+      productId,
+      message,
+      rating,
+    });
+
+    if (updatedReview) {
+      res.send({
+        success: true,
+        data: {
+          message: "Your review has been updated",
+          updatedReview,
+        },
+      });
+    } else {
+      next({
+        name: "ReviewError",
+        message: "Review didn't update",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 // DELETE /reviews/:productId
 
